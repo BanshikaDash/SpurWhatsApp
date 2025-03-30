e8f2a074a9a0a8269455fc68632b19c0c6975112
BanshikaDash <dashbanshika2005@gmail.com>
2025-03-30 10:57:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,6 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/SpurWhatsApp/', // Add this line for GitHub Pages
   plugins: [
     react(),
     mode === 'development' &&
